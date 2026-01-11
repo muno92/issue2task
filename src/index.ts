5ef74a549d0f37411e67aa249b209e20c61f50a6
n-ogawa <fy59.kou-729@live.jp>
2026-01-11 12:33:26 +0900

    Set .editorconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Hono } from 'hono'
-import { verifyGitHubSignature } from './middleware/verify-github-signature'
+import {Hono} from 'hono'
+import {verifyGitHubSignature} from './middleware/verify-github-signature'
 
 const app = new Hono()
 
@@ -8,13 +8,13 @@ app.get('/', (c) => {
 })
 
 app.post('/webhook/gh-issue-to-calendar-task', verifyGitHubSignature, async (c) => {
-    const eventName = c.req.header('X-GitHub-Event')
-    console.log(`Received event: ${eventName}`)
+  const eventName = c.req.header('X-GitHub-Event')
+  console.log(`Received event: ${eventName}`)
 
-    const payload = await c.req.json();
-    console.log('Received webhook payload:', payload.changes)
+  const payload = await c.req.json();
+  console.log('Received webhook payload:', payload.changes)
 
-    return c.text('Webhook received successfully')
+  return c.text('Webhook received successfully')
 })
 
 export default app
